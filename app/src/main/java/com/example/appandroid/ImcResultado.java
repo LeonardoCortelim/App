1830e18091d0835fabf38758a1507a317dd8851a
LeonardoCortelim <leo.cortelim@gmail.com>
2025-09-15 09:12:14 -0300

    Update imc

--- app/src/main/java/com/example/appandroid/ImcResultado.java
+++ app/src/main/java/com/example/appandroid/ImcResultado.java
@@ -1,5 +1,6 @@
 package com.example.appandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,11 @@ public class ImcResultado extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imc_resultado);
+        Intent intent =getIntent();
+        Bundle b=intent.getExtras();
+        Double peso=b.getDouble("peso");
+        Double altura=b.getDouble("altura");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -11,26 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.appandroid.ImcResultado;
-import com.example.appandroid.R;
-
 public class MainActivity extends AppCompatActivity {
     Button button;
-    EditText edPeso,edAltura;
+    EditText edPeso, edAltura;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.button4);
-        edPeso=findViewById(R.id.edPeso);
+        edPeso = findViewById(R.id.edPeso);
+        edAltura = findViewById(R.id.edAltura);
         button.setOnClickListener(v -> {
             Intent intent = new Intent(this, ImcResultado.class);
-            Bundle bundle= new Bundle();
-            Double peso= Double.parseDouble(edPeso.getText().toString());
-            Double altura= Double.parseDouble(edAltura.getText().toString());
-            bundle.putDouble("peso",peso);
-            bundle.putDouble("altura",altura);
+            Bundle bundle = new Bundle();
+            Double peso = Double.parseDouble(edPeso.getText().toString());
+            Double altura = Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("peso", peso);
+            bundle.putDouble("altura", altura);
+            intent.putExtras(bundle);
             startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
