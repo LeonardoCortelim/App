67c349194cc33efd480ec25bb72465d381bf0e6e
LeonardoCortelim <leo.cortelim@gmail.com>
2025-09-15 08:37:21 -0300

    Update imc

--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +16,21 @@ import com.example.appandroid.R;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    EditText edPeso,edAltura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.button4);
+        edPeso=findViewById(R.id.edPeso);
         button.setOnClickListener(v -> {
             Intent intent = new Intent(this, ImcResultado.class);
+            Bundle bundle= new Bundle();
+            Double peso= Double.parseDouble(edPeso.getText().toString());
+            Double altura= Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("peso",peso);
+            bundle.putDouble("altura",altura);
             startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
