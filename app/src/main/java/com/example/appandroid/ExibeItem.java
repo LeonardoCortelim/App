722b652795ffee509820626d9ab53b658f5b69cf
leonardo <leo.cortelim@gmail.com>
2025-11-17 08:42:03 -0300

    Nome aparecendo na tela 2 apos clicar sobre ele

--- app/src/main/java/com/example/appandroid/ExibeItem.java
+++ app/src/main/java/com/example/appandroid/ExibeItem.java
@@ -1,6 +1,7 @@
 package com.example.appandroid;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,8 @@ public class ExibeItem extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        String titulo = getIntent().getStringExtra("titulo");
+        TextView textViewTitulo = findViewById(R.id.textViewExibeItem);
+        textViewTitulo.setText(titulo);
     }
 }
