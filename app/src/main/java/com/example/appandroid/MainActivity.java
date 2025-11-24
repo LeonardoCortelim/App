be8d812e0174c8958a931bae300b4fd7d7909419
LeonardoCortelim <leo.cortelim@gmail.com>
2025-11-24 09:05:12 -0300

    Sensor

--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -6,35 +6,47 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import com.example.appandroid.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     SensorManager sm;
     TextView textView;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.textViewSensor);
-
-
-        sm =(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        textView = findViewById(R.id.textViewSensor);
+        listView = findViewById(R.id.listView);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor sensor = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sm.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+        List<Sensor> sensorList = sm.getSensorList(Sensor.TYPE_ALL);
+        ArrayList<String> listNameSensor = new ArrayList<>();
+        for (Sensor s: sensorList) {
+            listNameSensor.add(s.getName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listNameSensor);
+        listView.setAdapter(adapter);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         event.sensor.getName();
         textView.setText(Float.toString(event.values[0]));
-
     }
 
     @Override
